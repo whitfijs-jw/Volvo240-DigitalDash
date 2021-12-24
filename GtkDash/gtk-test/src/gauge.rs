3349f32ede2226bed805bf370d62210ae0188761
Jonathan Whitfield <jon@volumetrix.com>
2021-12-23 20:42:15 -0500

    - added value to gauge 	- added labels to gauge ticks 	-

--- GtkDash/gtk-test/src/gauge.rs
+++ GtkDash/gtk-test/src/gauge.rs
@@ -1,17 +1,18 @@
 use gtk::prelude::*;
 use glib::SignalHandlerId;
-use gtk::{Application, ApplicationWindow, Button, DrawingArea};
+use gtk::{DrawingArea};
 use std::rc::Rc;
 use std::cell::RefCell;
+use std::ops::Range;
+use pango::FontDescription;
 
 pub trait Draw<T> {
     fn draw(
-        area: &gtk::DrawingArea,
-        drawable: T
+        drawable: Rc<RefCell<T>>
     ) -> SignalHandlerId;
 }
 
-#[derive(Debug, Copy, Clone)]
+#[derive(Debug, Clone)]
 pub struct Gauge {
     outer: f64,
     inner: f64,
@@ -20,10 +21,12 @@ pub struct Gauge {
     theta_start: isize,
     theta_end: isize,
     step: usize,
+    step_angle: usize,
     min: f64,
     max: f64,
     pub val: f64,
     dial_diameter: f64,
+    drawing_area: DrawingArea,
 }
 impl Gauge {
 
@@ -38,9 +41,10 @@ impl Gauge {
         max: f64,
         step: usize,
         tick_width: f64,
+        drawing_area: DrawingArea,
     ) -> Gauge {
 
-        return Gauge {
+        let g = Gauge {
             outer: radius,
             inner: radius - tick_width,
             xc: xc,
@@ -48,11 +52,15 @@ impl Gauge {
             theta_start: 180 - end_angle,
             theta_end: 180 - start_angle,
             step: step, 
+            step_angle: (end_angle - start_angle) as usize * step / (max - min) as usize,
             min: min,
             max: max, 
             val: 0.0, 
-            dial_diameter: dial_diameter
+            dial_diameter: dial_diameter,
+            drawing_area: drawing_area,
         };
+
+        return g;
     }
 
     pub fn set_value(&mut self, val: f64) {
@@ -63,9 +71,15 @@ impl Gauge {
         } else {
             self.val = self.min;
         }
+
+        self.drawing_area.queue_draw();
     }
 
-    fn draw_internal(&self, c: &cairo::Context) {
+    pub fn get_range(&self) -> Range<f64> {
+        return Range {start: self.min, end: self.max};
+    }
+
+    pub fn draw_internal(&self, c: &cairo::Context, w: &DrawingArea) {
         c.set_source_rgb(1.0, 1.0, 1.0);
         c.paint();
 
@@ -74,40 +88,66 @@ impl Gauge {
         c.set_line_width(2.0);
         c.fill();
 
-        for theta in (self.theta_start..=self.theta_end).step_by(self.step) {
+        let mut font = FontDescription::new();
+        font.set_family("Monospace");
+        font.set_weight(pango::Weight::Semibold);
+        font.set_size(10 * pango::SCALE);
+
+        let num_steps = (self.max - self.min) as usize / self.step;
+        for theta in (self.theta_start..=self.theta_end).step_by(self.step_angle) {
             let a = theta as f64 * std::f64::consts::PI / 180.0;
 
             c.move_to(self.xc + (self.outer * a.cos()) , self.yc - (self.outer * a.sin()));
             c.line_to(self.xc + (self.inner * a.cos()), self.yc - (self.inner * a.sin()));
+
+            let val = self.max as isize - (((theta) - self.theta_start) * (self.max - self.min) as isize  / (self.theta_end - self.theta_start));
+            let v = (val).to_string();
+            let layout = w.create_pango_layout(Some(&v));
+            layout.set_font_description(Some(&font));
+
+            let (w, mut h) = layout.pixel_size();
+
+            if theta > 0 && theta < 180 {
+                h = 0;
+            }
+
+            c.move_to(self.xc + (self.inner * a.cos()) - (w as f64 / 2.0), self.yc - (self.inner * a.sin()) - h as f64);
+            pangocairo::show_layout(c, &layout);
+
         }
 
         c.stroke();
 
         c.move_to(self.xc, self.yc);
         c.set_source_rgb(1.0, 0.05, 0.05);
-        c.set_line_width(10.0);
+        c.set_line_width(self.outer / 2.0 / 10.0);
         
-        let pct = self.val / (self.max - self.min);
+        let pct = (self.val - self.min) / (self.max - self.min);
 
         let d = self.theta_end as f64 - ((self.theta_end - self.theta_start) as f64 * pct);
         let a = d * std::f64::consts::PI / 180.0;
 
-
         c.line_to(self.xc + (self.outer * a.cos()) , self.yc - (self.outer * a.sin()));
         c.stroke();
+
+        let val = format!("{:.2}", self.val);
+        let layout = w.create_pango_layout(Some(&val));
+        layout.set_font_description(Some(&font));
+        let (w, h) = layout.pixel_size();
+
+        c.move_to(self.xc - (w as f64 / 2.0), self.yc + self.dial_diameter);
+        pangocairo::show_layout(c, &layout);
     }
 }
 
 impl Draw<Gauge> for Gauge {
     fn draw(
-        area: &gtk::DrawingArea,
-        gauge: Gauge
+        gauge: Rc<RefCell<Gauge>>
     ) -> SignalHandlerId {
 
-        let rc_gauge = Rc::new(gauge);
-
-        let handler = area.connect_draw( move |w, c| {
-            rc_gauge.draw_internal(c);
+        let g = Rc::clone(&gauge);
+        let handler = gauge.borrow().drawing_area.connect_draw( move |w, c| {
+            g.borrow().draw_internal(c, w);
             
             w.queue_draw();
 
