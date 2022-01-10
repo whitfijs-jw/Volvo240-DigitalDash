83d4404373e7b754fd1bdd2be7ef5b900ae49c48
Jonathan Whitfield <jonathan.s.whitfield@gmail.com>
2022-01-09 22:12:34 -0500

    - updated post-build.sh script to remove all absolute paths so it doesn't suck to build everything 	- buildroot make will now build and package the Qt dash app in to the generated image -- no need to build it separately anymore 	- added more information to the README.md about how to setup buildroot and initial setup of the pi -- a lot more needed

--- buildroot/board/volvodash/post-build.sh
+++ buildroot/board/volvodash/post-build.sh
@@ -4,10 +4,17 @@ set -u
 set -e
 
 echo "we are here: $PWD"
+echo "host dir is: ${HOST_DIR}"
 
-QT_PROJECT_DIR="../QtDash"
-QT_EXEC_DIR="${QT_PROJECT_DIR}/VolvoDigitalDashModels/build-RPI3/app/VolvoDigitalDashModels"
-QT_FONTS_DIR="${QT_PROJECT_DIR}/VolvoDigitalDashModels/app/HandelGothReg.ttf"
+BUILDROOT_DIR="${PWD}"
+PROJECT_DIR="../QtDash"
+QT_PROJECT_DIR="${PROJECT_DIR}/VolvoDigitalDashModels"
+QMAKE_CMD="${HOST_DIR}/bin/qmake"
+QT_PROJECT_FILE=""
+QT_BUILD_CMD="${QMAKE_CMD} ${QT_PROJECT_DIR}/subdirs.pro -spec devices/linux-buildroot-g++ CONFIG+=debug CONFIG+=qml_debug CONFIG+=RPI && /usr/bin/make qmake_all"
+QT_BUILD_DIR="${QT_PROJECT_DIR}/build-RPI3/"
+QT_EXEC_DIR="${QT_PROJECT_DIR}/build-RPI3/app/VolvoDigitalDashModels"
+QT_FONTS_DIR="${QT_PROJECT_DIR}/app/HandelGothReg.ttf"
 QT_EXEC_TARGET_DIR="${TARGET_DIR}/opt"
 QT_FONTS_TARGET_DIR="${TARGET_DIR}/usr/lib/fonts"
 BOARD_DIR="$(dirname $0)"
@@ -20,7 +27,17 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
+echo "Create Qt App build directory"
+mkdir -p "${QT_BUILD_DIR}"
+
+echo "Building Qt App (qmake + make)"
+cd "${QT_BUILD_DIR}"
+${QMAKE_CMD} ../subdirs.pro -spec devices/linux-buildroot-g++ CONFIG+=debug CONFIG+=qml_debug CONFIG+=RPI && /usr/bin/make qmake_all
+
+make -j
+
 echo "Copying QtDash executable to /opt"
+cd "${BUILDROOT_DIR}"
 cp "${QT_EXEC_DIR}" "${QT_EXEC_TARGET_DIR}"
 
 echo "Copy custom config.txt"
@@ -33,4 +50,4 @@ echo "Copying mcp3208 dtb"
 cp "${BOARD_DIR}/mcp3208.dtbo" "${BINARIES_DIR}/rpi-firmware/overlays"
 
 echo "Copying Config from QtDash"
-cp "${QT_PROJECT_DIR}/config.ini" "${QT_EXEC_TARGET_DIR}"
+cp "${PROJECT_DIR}/config.ini" "${QT_EXEC_TARGET_DIR}"
