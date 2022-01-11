dab9c554ed4b7945bdd0bd92a56c0d6fe6047e7e
Jonathan Whitfield <jonathan.s.whitfield@gmail.com>
2022-01-10 23:41:00 -0600

    feature/tach_input_module: 	- made the path to the package a relative path in package/pulse_counter/pulse_counter.mk

--- buildroot/package/pulse_counter/pulse_counter.mk
+++ buildroot/package/pulse_counter/pulse_counter.mk
@@ -1,4 +1,4 @@
-PULSE_COUNTER_SITE = /home/whitfijs/git/Volvo240-DigitalDash/buildroot/package/pulse_counter
+PULSE_COUNTER_SITE = ./package/pulse_counter
 PULSE_COUNTER_SITE_METHOD = local
 $(eval $(kernel-module))
 $(eval $(generic-package))
