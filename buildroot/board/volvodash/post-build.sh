019cdeb87f7ed6525a4861b9a2b647bc75edca2a
Jonathan Whitfield <jonathan.s.whitfield@gmail.com>
2022-01-12 14:30:39 -0600

    - more kernel clean up 	- dropped rootfs size 	- disable console output on HDMI

--- buildroot/board/volvodash/post-build.sh
+++ buildroot/board/volvodash/post-build.sh
@@ -21,11 +21,11 @@ BOARD_DIR="$(dirname $0)"
 BOARD_NAME="$(basename ${BOARD_DIR})"
 
 # Add a console on tty1
-if [ -e ${TARGET_DIR}/etc/inittab ]; then
-    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
-	sed -i '/GENERIC_SERIAL/a\
-tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
-fi
+#if [ -e ${TARGET_DIR}/etc/inittab ]; then
+#    grep -qE '^tty1::' ${TARGET_DIR}/etc/inittab || \
+#	sed -i '/GENERIC_SERIAL/a\
+#tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
+#fi
 
 echo "Create Qt App build directory"
 mkdir -p "${QT_BUILD_DIR}"
