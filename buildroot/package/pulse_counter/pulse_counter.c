45855b53ba5c02e255d2b2994a9a7abbe6985023
Jonathan Whitfield <jonathan.s.whitfield@gmail.com>
2022-02-04 20:08:07 -0500

    bugfix/hardware-revA-issues:         - modified pulse_counter kernel module to handle both VSS and tach pulse inputs         - fixed issue with tach signal jumping around -- ignore pulses that are too fast instead of setting them to the minimum pulse spacing -- this also needs to be handled in hardware a little better at some point         - added splash screen update as early as possible -- before busybox init scripts start running 	- init is now pointed to /sbin/preinit which loads the frame buffer splash and then calls /sbin/init 	- TachInput class now points to /sys/class/volvodash/tach_counter (vss will point to /vss_counter) 	- TODO: update tach adapter with how the current board has to be modified -- possibly add schmitt trigger optocoupler like H11Lx

--- buildroot/package/pulse_counter/pulse_counter.c
+++ buildroot/package/pulse_counter/pulse_counter.c
@@ -17,12 +17,9 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("whitfijs");
 
 
-#define PULSE_COUNTER_GPIO                  6
-#define PULSE_COUNTER_GPIO_DESC             "Pulse Interrupt Pin"
-#define PULSE_COUNTER_GPIO_DEVICE_DESC      "Pulse counter"
-
-#define PULSE_COUNTER_LED_GPIO              18
-#define PULSE_COUNTER_LED_GPIO_DESC         "Pulse counter led indicator pin"
+#define TACH_PULSE_COUNTER_GPIO             6
+#define TACH_PULSE_COUNTER_GPIO_DESC        "Tach Pulse Interrupt Pin"
+#define TACH_PULSE_COUNTER_GPIO_DEVICE_DESC "Tach Pulse counter"
 
 #define PULSE_SPACING_NUM_SAMPLES           4
 #define MAX_PULSE_SPACING_NUM_SAMPLES		32
@@ -30,125 +27,197 @@ MODULE_AUTHOR("whitfijs");
 #define PULSE_SPACING_TIMEOUT_MSEC			200
 #define PULSE_SPACING_MIN_DEFAULT_USEC 		2500		
 
+#define VSS_PULSE_COUNTER_GPIO				19
+#define VSS_PULSE_COUNTER_GPIO_DESC			"VSS Pulse Interrupt Pin"
+#define VSS_PULSE_COUNTER_GPIO_DEVICE_DESC	"VSS Pulse Counter"
 
-static __u16 pulse_counter_gpio_irq_num 	= 0;
-static __u32 pulse_count_total				= 0;
-static __u64 pulse_spacing[MAX_PULSE_SPACING_NUM_SAMPLES];
-static __u64 pulse_spacing_index            = 0;
-static __u64 pulse_spacing_avg              = 0;
-static __u64 pulse_spacing_avg_num_samples  = PULSE_SPACING_NUM_SAMPLES;
-static __u64 pulse_spacing_min				= PULSE_SPACING_MIN_DEFAULT_USEC * 1000;
+static struct class *s_pDeviceClass;
 
-struct timer_list timeout_timer;
+static struct device *s_pTachDeviceObject;
+static struct device *s_pVssDeviceObject;
 
-void restart_timeout_timer(void) {
-	mod_timer (&timeout_timer, jiffies + ( msecs_to_jiffies(PULSE_SPACING_TIMEOUT_MSEC)));	
+typedef struct {
+	struct device * dev;
+	 __u16 gpio_irq_num;
+	 __u32 count_total;
+	 __u64 spacing[MAX_PULSE_SPACING_NUM_SAMPLES];
+	 __u64 spacing_index;
+	 __u64 spacing_avg;
+	 __u64 spacing_avg_num_samples;
+	 __u64 spacing_min;
+
+	 ktime_t last;
+	 int last_interrupt;
+	
+	struct timer_list timeout_timer;
+} pulse_counter_t;
+
+static pulse_counter_t tach_pulse_counter;
+static pulse_counter_t vss_pulse_counter;
+
+void init_pulse_counter(pulse_counter_t * counter, int num_avg_samples, int spacing_min) {
+	// set to default values
+	counter->gpio_irq_num = 0;
+	counter->count_total = 0;
+	counter->spacing_index = 0;
+	counter->spacing_avg = 0;
+	counter->spacing_avg_num_samples = num_avg_samples;
+	counter->spacing_min = spacing_min;
+}
+
+pulse_counter_t * get_counter_from_device(struct device * dev) {
+	if (dev == tach_pulse_counter.dev) {
+		return &tach_pulse_counter;
+	} else if (dev == vss_pulse_counter.dev) {
+		return &vss_pulse_counter;
+	}
+
+	return NULL;
+}
+
+void restart_timeout_timer(struct timer_list * timeout_timer, unsigned long timeout_msec) {
+	mod_timer (timeout_timer, jiffies + ( msecs_to_jiffies(timeout_msec)));	
 }
 
 void timeout_timer_function(struct timer_list *timer) {
+	pulse_counter_t * counter;
+
+	if (timer == &tach_pulse_counter.timeout_timer) {
+		counter = &tach_pulse_counter;
+	} else if (timer == &vss_pulse_counter.timeout_timer) {
+		counter = &vss_pulse_counter;
+	}
+
 	// reset pulse spacing
-	pulse_spacing_avg = 0;
-	pulse_spacing_index = 0;
+	counter->spacing_avg = 0;
+	counter->spacing_index = 0;
 	for (int i = 0; i < PULSE_SPACING_NUM_SAMPLES; i++) {
-		pulse_spacing[i] = 0;
+		counter->spacing[i] = 0;
 	}
 
 	// restart the timer
-	restart_timeout_timer();
+	restart_timeout_timer(timer, PULSE_SPACING_TIMEOUT_MSEC);
 }
 
-static irqreturn_t pulse_irq_handler(__u32 irq, void * dev_id, struct pt_regs * regs){
-	static ktime_t last;
+static irqreturn_t pulse_irq_handler(__u32 irq, void * dev_id, struct pt_regs * regs) {
+	pulse_counter_t * counter;
 	ktime_t now;
-	static int lastInterrupt = 0;
-    static __u8 on = 0;
-	
-	// temporary -- flip pin
-    on = !on;
-    gpio_set_value(PULSE_COUNTER_LED_GPIO, on);
 
-	// set the pulse spacing avg
-    if (lastInterrupt == 0) {
-		last = ktime_get();
-        lastInterrupt = jiffies;
-    } else {
+	if (irq == tach_pulse_counter.gpio_irq_num) {
+		counter = &tach_pulse_counter;
+	} else if (irq == vss_pulse_counter.gpio_irq_num) {
+		counter = &vss_pulse_counter;
+	} else {
+		// dip out
+		return IRQ_HANDLED;
+	}
+
+	if (counter->last_interrupt == 0) {
+		counter->last = ktime_get();
+		counter->last_interrupt = jiffies;
+	} else {
 		now = ktime_get();
-        __u64 now_nsec = ktime_to_ns(now);
-		__u64 last_nsec = ktime_to_ns(last);
+		__u64 now_nsec = ktime_to_ns(now);
+		__u64 last_nsec = ktime_to_ns(counter->last);
 		__u64 spacing = (now_nsec - last_nsec);
 
-		// if we've got someting erroneous skip it
-		if (spacing < pulse_spacing_min) {
-			spacing = pulse_spacing_min;
+		// skip too short pulses
+		if (spacing < counter->spacing_min) {
+			return IRQ_HANDLED;
+			spacing = counter->spacing_min;
 		}
-		 
-		pulse_spacing[pulse_spacing_index++] = spacing;
-		pulse_spacing_index = pulse_spacing_index & (pulse_spacing_avg_num_samples - 1);		
 
-		if (pulse_count_total > pulse_spacing_avg_num_samples) {
+		// accumulate
+		counter->spacing[counter->spacing_index++] = spacing;
+		counter->spacing_index = counter->spacing_index & (counter->spacing_avg_num_samples - 1);
+
+		if (counter->count_total++ > counter->spacing_avg_num_samples) {
 			__u64 avg = 0;
-			for (int i = 0; i < pulse_spacing_avg_num_samples; i++) {
-				avg += pulse_spacing[i];
+			for (int i = 0; i < counter->spacing_avg_num_samples; i++) {
+				avg += counter->spacing[i];
 			}
-			pulse_spacing_avg = div_u64(avg, pulse_spacing_avg_num_samples);
+			counter->spacing_avg = div_u64(avg, counter->spacing_avg_num_samples);
 		}
-		
 
-		last = now;
-    }
+		counter->last = now;
+	}	
 
-	// increment pulse count total
-    pulse_count_total++;
-
-	// reset the timeout timer
-	restart_timeout_timer();
+	restart_timeout_timer(&counter->timeout_timer, PULSE_SPACING_TIMEOUT_MSEC);
 
     return IRQ_HANDLED;
 }
 
-static ssize_t set_pulse_count_callback(struct device* dev,struct device_attribute* attr, const char * buf, size_t count){
+static ssize_t set_pulse_count_callback(struct device* dev ,struct device_attribute* attr, const char * buf, size_t count) {
+	pulse_counter_t * counter = get_counter_from_device(dev);
+
 	long int count_value = 0;
 	if (kstrtol(buf, 10, &count_value) < 0)
 		return -EINVAL;
 
-	pulse_count_total = count_value;
+	if (counter != NULL) {
+		counter->count_total = count_value;
+	}
+
 	return count;
 }
 
-static ssize_t show_pulse_count_callback(struct device *d, struct device_attribute * attr, char * buf){
-	__u32 pulse_count = pulse_count_total;
-	return sprintf(buf, "%u\n", pulse_count);
+static ssize_t show_pulse_count_callback(struct device *dev, struct device_attribute * attr, char * buf){
+	pulse_counter_t * counter = get_counter_from_device(dev);
+	if (counter != NULL) {
+		__u32 pulse_count = counter->count_total;
+		return sprintf(buf, "%u\n", pulse_count);
+	}
+
+	return 0;
 }
 
-static ssize_t set_pulse_spacing_avg_callback(struct device* dev,struct device_attribute* attr, const char * buf, size_t count){
+static ssize_t set_pulse_spacing_avg_callback(struct device * dev,struct device_attribute * attr, const char * buf, size_t count){
+	pulse_counter_t * counter = get_counter_from_device(dev);
 	long int spacing = 0;
 	if (kstrtol(buf, 10, &spacing) < 0)
 		return -EINVAL;
 
-	pulse_spacing_avg = spacing;
+	if (counter != NULL) {
+		counter->spacing_avg = spacing;
+	}
 	return count;
 }
 
-static ssize_t show_pulse_spacing_avg_callback(struct device *d, struct device_attribute * attr, char * buf){
-	__u64 pulse_spacing = pulse_spacing_avg;
-	return sprintf(buf, "%llu\n", pulse_spacing);
+static ssize_t show_pulse_spacing_avg_callback(struct device * dev, struct device_attribute * attr, char * buf) {
+	pulse_counter_t * counter = get_counter_from_device(dev);
+
+	if (counter != NULL) {
+		__u64 pulse_spacing = counter->spacing_avg;
+		return sprintf(buf, "%llu\n", pulse_spacing);
+	}
+
+	return 0;
 }
 
-static ssize_t set_min_pulse_spacing_callback(struct device* dev,struct device_attribute* attr, const char * buf, size_t count) {
+static ssize_t set_min_pulse_spacing_callback(struct device * dev,struct device_attribute * attr, const char * buf, size_t count) {
+	pulse_counter_t * counter = get_counter_from_device(dev);
 	long int spacing = 0;
 	if (kstrtol(buf, 10, &spacing) < 0)
 		return -EINVAL;
 
-	pulse_spacing_min = spacing;
+	if (counter != NULL) {
+		counter->spacing_min = spacing;
+	}
 	return count;
 }
 
-static ssize_t show_min_pulse_spacing_callback(struct device *d, struct device_attribute * attr, char * buf) {
-	__u64 spacing = pulse_spacing_min;
-	return sprintf(buf, "%llu\n", spacing);
+static ssize_t show_min_pulse_spacing_callback(struct device * dev, struct device_attribute * attr, char * buf) {
+	pulse_counter_t * counter = get_counter_from_device(dev);
+	if (counter != NULL) {
+		__u64 spacing = counter->spacing_min;
+		return sprintf(buf, "%llu\n", spacing);
+	}
+
+	return 0;
 }
 
-static ssize_t set_avg_num_samples_callback(struct device* dev,struct device_attribute* attr, const char * buf, size_t count) {
+static ssize_t set_avg_num_samples_callback(struct device* dev, struct device_attribute * attr, const char * buf, size_t count) {
+	pulse_counter_t * counter = get_counter_from_device(dev);
 	long int num_samples = 0;
 	if (kstrtol(buf, 10, &num_samples) < 0)
 		return -EINVAL;
@@ -169,17 +238,24 @@ static ssize_t set_avg_num_samples_callback(struct device* dev,struct device_att
 
 	printk("Num samples to average: %ld\n", v);
 
-	pulse_spacing_index = 0;
-	pulse_spacing_avg = 0;
-	pulse_count_total = 0;
+	if (counter != NULL) {
+		counter->spacing_index = 0;
+		counter->spacing_avg = 0;
+		counter->count_total = 0;
 
-	pulse_spacing_avg_num_samples = v;
+		counter->spacing_avg_num_samples = v;
+	}
 	return count;
 }
 
-static ssize_t show_avg_num_samples_callback(struct device *d, struct device_attribute * attr, char * buf) {
-	__u64 output = pulse_spacing_avg_num_samples;
-	return sprintf(buf, "%llu\n", output);
+static ssize_t show_avg_num_samples_callback(struct device * dev, struct device_attribute * attr, char * buf) {
+	pulse_counter_t * counter = get_counter_from_device(dev);
+	if (counter != NULL) {
+		__u64 output = counter->spacing_avg_num_samples;
+		return sprintf(buf, "%llu\n", output);
+	}
+
+	return 0;
 }
 
 static DEVICE_ATTR(pulse_count, 00664, show_pulse_count_callback, set_pulse_count_callback);
@@ -187,76 +263,111 @@ static DEVICE_ATTR(pulse_spacing_avg, 00664, show_pulse_spacing_avg_callback, se
 static DEVICE_ATTR(pulse_spacing_min, 00664, show_min_pulse_spacing_callback, set_min_pulse_spacing_callback);
 static DEVICE_ATTR(pulse_spacing_avg_num_samples, 00664, show_avg_num_samples_callback, set_avg_num_samples_callback);
 
-static struct class *s_pDeviceClass;
-static struct device *s_pDeviceObject;
-
 static int __init pulseCounterModule_init(void){
 	int result;
-	
-	if (gpio_request(PULSE_COUNTER_LED_GPIO, "LED Pin")) {
-	  printk("GPIO request faiure: %s\n", "LED Pin");
-	  return -EINVAL;
-	}
-	gpio_direction_output(PULSE_COUNTER_LED_GPIO, 1);
+
+	init_pulse_counter(&tach_pulse_counter, PULSE_SPACING_NUM_SAMPLES, PULSE_SPACING_MIN_DEFAULT_USEC * 1000);
+	init_pulse_counter(&vss_pulse_counter, PULSE_SPACING_NUM_SAMPLES, PULSE_SPACING_MIN_DEFAULT_USEC * 1000);
     
     // create class in /sys/class
 	s_pDeviceClass = class_create(THIS_MODULE, "volvo_dash");
 	BUG_ON(IS_ERR(s_pDeviceClass));
     
-    // create pulse_count attribute
-	s_pDeviceObject = device_create(s_pDeviceClass, NULL, 0, NULL, "pulse_counter");
-	BUG_ON(IS_ERR(s_pDeviceObject));
+    // create tach pulse counting attribute
+	s_pTachDeviceObject = device_create(s_pDeviceClass, NULL, 0, NULL, "tach_counter");
+	BUG_ON(IS_ERR(s_pTachDeviceObject));
 
-	result = device_create_file(s_pDeviceObject, &dev_attr_pulse_count);
-	result = device_create_file(s_pDeviceObject, &dev_attr_pulse_spacing_avg);
-	result = device_create_file(s_pDeviceObject, &dev_attr_pulse_spacing_min);
-	result = device_create_file(s_pDeviceObject, &dev_attr_pulse_spacing_avg_num_samples);
+	result = device_create_file(s_pTachDeviceObject, &dev_attr_pulse_count);
+	result = device_create_file(s_pTachDeviceObject, &dev_attr_pulse_spacing_avg);
+	result = device_create_file(s_pTachDeviceObject, &dev_attr_pulse_spacing_min);
+	result = device_create_file(s_pTachDeviceObject, &dev_attr_pulse_spacing_avg_num_samples);
+	tach_pulse_counter.dev = s_pTachDeviceObject;
 
-   if (gpio_request(PULSE_COUNTER_GPIO, PULSE_COUNTER_GPIO_DESC)) {
-      printk("GPIO request faiure: %s\n", PULSE_COUNTER_GPIO_DESC);
-      return -EINVAL;
-   }
-   
- 
-   if ( (pulse_counter_gpio_irq_num = gpio_to_irq(PULSE_COUNTER_GPIO)) < 0 ) {
-      printk("GPIO to IRQ mapping faiure %s\n", PULSE_COUNTER_GPIO_DESC);
-      return -EINVAL;
-   }
- 
-   printk(KERN_NOTICE "Mapped int %d\n", pulse_counter_gpio_irq_num);
- 
-   if (request_irq(pulse_counter_gpio_irq_num,
-                   (irq_handler_t ) pulse_irq_handler,
-                   IRQF_TRIGGER_FALLING,
-                   PULSE_COUNTER_GPIO_DESC,
-                   PULSE_COUNTER_GPIO_DEVICE_DESC)) {
-      printk("Irq Request failure\n");
-      return -EINVAL;
-   }
+	// create vss pulse counting attribute
+	s_pVssDeviceObject = device_create(s_pDeviceClass, NULL, 0, NULL, "vss_counter");
+	BUG_ON(IS_ERR(s_pVssDeviceObject));
 
+	result = device_create_file(s_pVssDeviceObject, &dev_attr_pulse_count);
+	result = device_create_file(s_pVssDeviceObject, &dev_attr_pulse_spacing_avg);
+	result = device_create_file(s_pVssDeviceObject, &dev_attr_pulse_spacing_min);
+	result = device_create_file(s_pVssDeviceObject, &dev_attr_pulse_spacing_avg_num_samples);
+	vss_pulse_counter.dev = s_pVssDeviceObject;
 
-	timer_setup(&timeout_timer, timeout_timer_function, 0);
-	restart_timeout_timer();
+	// Request GPIOS
+	if (gpio_request(TACH_PULSE_COUNTER_GPIO, TACH_PULSE_COUNTER_GPIO_DESC)) {
+		printk("GPIO request faiure: %s\n", TACH_PULSE_COUNTER_GPIO_DESC);
+		return -EINVAL;
+	}
+
+	if (gpio_request(VSS_PULSE_COUNTER_GPIO, VSS_PULSE_COUNTER_GPIO_DESC)) {
+		printk("GPIO request faiure: %s\n", VSS_PULSE_COUNTER_GPIO_DESC);
+		return -EINVAL;
+	}
+
+	//setup IRQs
+	if ( (tach_pulse_counter.gpio_irq_num = gpio_to_irq(TACH_PULSE_COUNTER_GPIO)) < 0 ) {
+		printk("GPIO to IRQ mapping faiure %s\n", TACH_PULSE_COUNTER_GPIO_DESC);
+		return -EINVAL;
+	}
+	printk(KERN_NOTICE "%s Mapped int %d\n", TACH_PULSE_COUNTER_GPIO_DESC, tach_pulse_counter.gpio_irq_num);
+
+	if (request_irq(tach_pulse_counter.gpio_irq_num,
+					(irq_handler_t ) pulse_irq_handler,
+					IRQF_TRIGGER_FALLING,
+					TACH_PULSE_COUNTER_GPIO_DESC,
+					(void *)&tach_pulse_counter)) {
+		printk("Irq Request failure\n");
+		return -EINVAL;
+	}
+
+	if ( (vss_pulse_counter.gpio_irq_num = gpio_to_irq(VSS_PULSE_COUNTER_GPIO)) < 0 ) {
+		printk("GPIO to IRQ mapping faiure %s\n", VSS_PULSE_COUNTER_GPIO_DESC);
+		return -EINVAL;
+	}
+	printk(KERN_NOTICE "%s Mapped int %d\n", VSS_PULSE_COUNTER_GPIO_DESC, vss_pulse_counter.gpio_irq_num);
+
+	if (request_irq(vss_pulse_counter.gpio_irq_num,
+					(irq_handler_t ) pulse_irq_handler,
+					IRQF_TRIGGER_FALLING,
+					VSS_PULSE_COUNTER_GPIO_DESC,
+					(void* )&vss_pulse_counter)) {
+		printk("Irq Request failure\n");
+		return -EINVAL;
+	}
+
+	//&tach_pulse_counter.timeout_timer.data = &tach_pulse_counter;
+	timer_setup(&tach_pulse_counter.timeout_timer, timeout_timer_function, 0);
+	restart_timeout_timer(&tach_pulse_counter.timeout_timer, PULSE_SPACING_TIMEOUT_MSEC);
+
+	timer_setup(&vss_pulse_counter.timeout_timer, timeout_timer_function, 0);
+	restart_timeout_timer(&vss_pulse_counter.timeout_timer, PULSE_SPACING_TIMEOUT_MSEC);
 
 	return 0;
 }
 
 static void __exit pulseCounterModule_exit(void) {
-	del_timer(&timeout_timer);
+	del_timer(&tach_pulse_counter.timeout_timer);
+	del_timer(&vss_pulse_counter.timeout_timer);
 
-	device_remove_file(s_pDeviceObject, &dev_attr_pulse_count);
-	device_remove_file(s_pDeviceObject, &dev_attr_pulse_spacing_avg);
-	device_remove_file(s_pDeviceObject, &dev_attr_pulse_spacing_min);
-	device_remove_file(s_pDeviceObject, &dev_attr_pulse_spacing_avg_num_samples);
+	device_remove_file(s_pTachDeviceObject, &dev_attr_pulse_count);
+	device_remove_file(s_pTachDeviceObject, &dev_attr_pulse_spacing_avg);
+	device_remove_file(s_pTachDeviceObject, &dev_attr_pulse_spacing_min);
+	device_remove_file(s_pTachDeviceObject, &dev_attr_pulse_spacing_avg_num_samples);
+
+	device_remove_file(s_pVssDeviceObject, &dev_attr_pulse_count);
+	device_remove_file(s_pVssDeviceObject, &dev_attr_pulse_spacing_avg);
+	device_remove_file(s_pVssDeviceObject, &dev_attr_pulse_spacing_min);
+	device_remove_file(s_pVssDeviceObject, &dev_attr_pulse_spacing_avg_num_samples);
+
 	device_destroy(s_pDeviceClass, 0);
 	class_destroy(s_pDeviceClass);
 
-	/*Free LED GPIO*/
-	gpio_free(PULSE_COUNTER_LED_GPIO);
+	/*Free pulse counter interrupts and GPIO*/
+	free_irq(tach_pulse_counter.gpio_irq_num, TACH_PULSE_COUNTER_GPIO_DEVICE_DESC);
+	gpio_free(TACH_PULSE_COUNTER_GPIO);
 
-	/*Free pulse counter interrupt and GPIO*/
-	free_irq(pulse_counter_gpio_irq_num, PULSE_COUNTER_GPIO_DEVICE_DESC);
-	gpio_free(PULSE_COUNTER_GPIO);
+	free_irq(vss_pulse_counter.gpio_irq_num, VSS_PULSE_COUNTER_GPIO_DEVICE_DESC);
+	gpio_free(VSS_PULSE_COUNTER_GPIO);
 }
 
 module_init(pulseCounterModule_init);
